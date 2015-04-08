44b43625e1c7aa1e7c96685d818514585c84fed8
Karissa McKelvey <krmckelv@gmail.com>
2015-04-08 00:02:50 -0700

    Remove prototype inherits call

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 var SetStream = require('sorted-set-stream')
 var util = require('util')
 
-var Union = function (a, b, toKey) {
-  if (!(this instanceof Union)) return new Union(a, b, toKey)
-  SetStream.call(this, a, b, toKey)
-}
 
-util.inherits(Union, SetStream)
+module.exports = function (a, b, toKey) {
+  var stream = SetStream(a, b, toKey)
+  stream.next = function (keys, vals, consumes) {
+    var self = this
+    if (!vals || (!vals.a && !vals.b)) return self.push(null)
 
-Union.prototype.setFunction = function (keys, vals, consumes) {
-  var self = this
-  if (!vals || (!vals.a && !vals.b)) return self.push(null)
+    if (!vals.a) {
+      consumes.b()
+      return self.push(vals.b)
+    }
 
-  if (!vals.a) {
+    if (!vals.b) {
+      consumes.a()
+      return self.push(vals.a)
+    }
+
+    if (keys.a === keys.b) {
+      consumes.b()
+      return self._read()
+    }
+
+    if (keys.a < keys.b) {
+      consumes.a()
+      return self.push(vals.a)
+    }
+
     consumes.b()
-    return self.push(vals.b)
+    self.push(vals.b)
   }
-
-  if (!vals.b) {
-    consumes.a()
-    return self.push(vals.a)
-  }
-
-  if (keys.a === keys.b) {
-    consumes.b()
-    return self._read()
-  }
-
-  if (keys.a < keys.b) {
-    consumes.a()
-    return self.push(vals.a)
-  }
-
-  consumes.b()
-  self.push(vals.b)
+  return stream
 }
-
-module.exports = Union
